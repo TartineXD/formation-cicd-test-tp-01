f2ddb6c6c35f6fe519ab161530de0e9923a126a9
Violette CROCHET <violette.crochet2024@campus-eni.fr>
2026-01-19 12:23:16 +0100

    startproject

---
+++ order-service/src/main/java/com/devops/cicd/PricingConfig.java
@@ -0,0 +1,18 @@
+package com.devops.cicd;
+
+public class PricingConfig {
+    private final double vatRate;
+    private final double freeShippingThreshold;
+
+    public PricingConfig(double vatRate, double freeShippingThreshold) {
+        //TODO
+    }
+
+    public double getVatRate() {
+        //TODO
+    }
+
+    public double getFreeShippingThreshold() {
+        //TODO
+    }
+}
