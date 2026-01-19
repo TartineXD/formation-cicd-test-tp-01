018ff1b53e4e42097a2ac484b6d9f9f7fbef8a16
Violette CROCHET <violette.crochet2024@campus-eni.fr>
2026-01-19 13:44:48 +0100

    reset

--- order-service/src/test/java/com/devops/cicd/PricingServiceTest.java
+++
@@ -1,7 +0,0 @@
-class PricingServiceTest {
-
-    private final PricingConfig fakeConfig = new PricingConfig(20.0, 50.0);
-    private final PricingService service = new PricingService(fakeConfig);
-    
-    //TODO
-}
